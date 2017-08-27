33b666bbffb1571982bfa9c04ac6acbaa7e838fd
keyur1992 <30588756+keyur1992@users.noreply.github.com>
2017-08-28 04:10:41 +1000

    Add files via upload
    delete.php

---
+++ demo/delete.php
@@ -0,0 +1,23 @@
+<?php
+/* This is a file which abstracts the DB connecting functionality */
+include("db_connection.php");
+/* These are the smarty files */
+require '..\libs\Smarty.class.php';
+$smarty = new Smarty;
+/* Define Variable */
+$id = '';
+ 
+if(isset($_POST['id'])) 
+{
+	$id = $_POST['id']; 
+} 
+if(isset($_POST['delete']))
+{    
+	/* delete */ 
+	$result = mysql_query("delete from  student WHERE id='$id' ") or die(mysql_error()); 
+	if($result)
+	{
+		echo "<script>window.location='index.php';</script>";
+	}
+}		
+?>
