8142d30a252a08144f826764d76d1fad3aaad027
Priya <Priya31793@gmail.com>
2017-08-28 13:41:27 +1000

    deleted config file

--- demo/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$dbHost = "localhost";
-$dbUser = "root";
-$dbPassword="";
-$dbName="test";
-$con = mysql_connect($dbHost,$dbUser,$dbPassword); 
-$sel = mysql_select_db($dbName,$con) or mysql_error();
-?>
