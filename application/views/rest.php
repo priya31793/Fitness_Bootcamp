b40cf1493997d572555892ac0182d661df45d5bf
keyur1992 <30588756+keyur1992@users.noreply.github.com>
2017-10-16 09:56:18 +0530

    Update rest.php
    comments

--- application/views/rest.php
+++ application/views/rest.php
@@ -2,9 +2,9 @@
 <?php 
 	header('Content-Type: application/json');
 
-  if(isset($_GET['id'])){
-  	$var = $_GET['id'];
-  	  $con=mysqli_connect("localhost","root","","test");
+  if(isset($_GET['id'])){//get method implementation. 
+  	$var = $_GET['id'];// varible declaration
+  	  $con=mysqli_connect("localhost","root","","test");//database connection
 	  if (mysqli_connect_errno()){
 	  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	  }
@@ -12,7 +12,7 @@
 	if($result = mysqli_query($con, $sql)){
 	    if(mysqli_num_rows($result) > 0){
 	    	while($row = mysqli_fetch_array($result)){
-	    		echo json_encode($row, JSON_PRETTY_PRINT);
+	    		echo json_encode($row, JSON_PRETTY_PRINT);//result data json encode
 	    	}
 	    }
 	}
