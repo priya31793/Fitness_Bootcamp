2e57764f29ca4489283aed849a300a24846d885a
keyur1992 <30588756+keyur1992@users.noreply.github.com>
2017-10-16 09:57:50 +0530

    Update client.php

--- application/views/client.php
+++ application/views/client.php
@@ -1,8 +1,8 @@
 <?php
 	if(isset($_GET['id'])){
-		$url = file_get_contents("http://localhost/week6/rest.php?id=".$_GET['id']);
-		$var = json_decode($url,true);
-		echo "<table>";
+		$url = file_get_contents("http://localhost/week6/rest.php?id=".$_GET['id']);//get link for consuming to json.//if upload at server change in url
+		$var = json_decode($url,true);//json data decode to normal value.
+		echo "<table>";//data show on table.
 			foreach($var as $k=>$v)
     		echo "<tr><td>$k</td><td>$v</td></tr>";
 		echo "</table>";
